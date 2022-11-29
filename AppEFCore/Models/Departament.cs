8a751c42372f698b54877448a35cbd737a7c8fbb
jonh.carvalho <jonh.carvalho@prof.infnet.edu.br>
2022-11-29 08:53:08 -0300

    Add project files.

---
+++ AppEFCore/Models/Departament.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppEFCore.Models
+{
+    public partial class Departament
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ AppEFCore/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppEFCore.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public int DepartmentId { get; set; }
+        public string Name { get; set; } = null!;
+        public string Designation { get; set; } = null!;
+    }
+}
